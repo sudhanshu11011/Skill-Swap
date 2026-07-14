14301c78a502ff75cd9162954832dfea89c1ecbe
swayamjain1240 <swayamjain1240@gmail.com>
2026-07-14 19:11:50 +0530

    backend2

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,12 @@
 import express from "express";
+import Dotenv  from "dotenv";
+Dotenv.config();
+
+import authRoute from "./router/authRoute.js"
 
 const app = express.Router() ;
 
+
+app.use("/api/auth", authRoute);
 
 export default router ;
