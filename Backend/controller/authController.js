fc508cb8b4bc3a6aab89e8d135dac1465048b91f
swayamjain1240 <swayamjain1240@gmail.com>
2026-07-16 19:04:35 +0530

    backend 7

--- Backend/controller/authController.js
+++ Backend/controller/authController.js
@@ -1,5 +1,6 @@
-import express from "express"
-import userModel from "../model/userModel.js"
+import express from "express";
+import User from "../model/userModel.js";
+import jwt from "jsonwebtoken";
 
 export const login = async(req,res)=>{
     try {
@@ -7,6 +8,29 @@ export const login = async(req,res)=>{
         if(!email || !password){
             return res.status(400).json({message:"All field Required"})
         }
+
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(401).json({message:"Account Not Found"});
+        }
+
+        const isPassCorrect = await User.matchPassword(password);
+        if(!isPassCorrect){
+            return res.status(401).json({message:"Invalid EmailId and Password"});
+        }
+
+        const token = jwt.sign({userId: user._id}, process.env.JWT_TOKEN,{
+            expiresIn:"7d",
+        })
+
+        req.cookie("jwt",token,{
+            maxAge: 7*24*60*60*1000,
+            httpOnly:true,
+            sameSite:"strict",
+            secure:process.env.NODE_ENV === "production",
+        });
+
+        res.status(200).json({success: true})
 
         
     } catch (error) {
@@ -16,7 +40,7 @@ export const login = async(req,res)=>{
 
 export const singup = async (req,res) => {
     try {
-        
+        const {email, password, fullName} = req.body ;
     } catch (error) {
         
     }
